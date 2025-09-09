461c7ee5ce5bb24088bf791a70084431bc454663
ouamarawl <ouamara.wail8@gmail.com>
2025-09-09 14:17:50 +0200

    new images

--- frontend/src/Pages/larriere_cuisine/Larriere_cuisine.js
+++ frontend/src/Pages/larriere_cuisine/Larriere_cuisine.js
@@ -6,6 +6,13 @@ import image2 from "../../assets/image2.jpg";
 import image3 from "../../assets/image3.jpg";
 import image4 from "../../assets/image4.jpg";
 import feille from "../../assets/feuille.png";
+import image8 from "../../assets/image8.jpg";
+import image12 from "../../assets/image12.jpg";
+import image13 from "../../assets/image13.jpg";
+import image14 from "../../assets/image14.jpg";
+import image15 from "../../assets/image15.jpg";
+import image9 from "../../assets/image9.jpg";
+
 function Larriere_cuisine() {
   return (
     <div className="larriere-cuisine-container">
@@ -13,9 +20,7 @@ function Larriere_cuisine() {
         <h3>L’Arrière Cuisine</h3>
         <img src={feille} />
       </div>
-      {/* <img src={image1} />
-            <img src={image2} />
-            <img src={image3} /> */}
+
       <h3
         style={{
           color: "#849D75",
@@ -39,7 +44,29 @@ function Larriere_cuisine() {
         className="chef-signature"
       />
 
-      <section className="A_decouvrir">
+      <section className="section-oeuvres">
+        <img src={image15} id="image15" />
+        <img src={image9} id="image9" />
+        <img src={image14} id="image14" />
+        <img src={image12}  id="image12"/>
+        <img src={image13}  id="image13" />
+        
+        
+        <div className="presentation-text">
+          <h3>LES ŒUVREs DU CHEF</h3>
+          <p>
+            Collages, lithographies, bronzes, assiettes… Les oeuvres du Chef
+            Alain Passard sont une traduction en image de sa passion du beau
+            geste : un jeu de couleurs, de textures que l’on retrouve aussi bien
+            sous cadre que dans l’assiette. Ils traduisent toute l’influence des
+            légumes dans la cuisine du Chef, l’importance de l’oeil dans la
+            création d’une recette.
+          </p>
+          <button className="discover-btn">En savoir plus</button>
+        </div>
+      </section>
+
+      {/* <section className="A_decouvrir">
         <div className=" A_decouvrir-text">
           <div className=" A_decouvrir-text-content">
             <h1>
@@ -61,7 +88,7 @@ function Larriere_cuisine() {
           <div class="ligne"></div>
         </div>
         <img src={image1} />
-      </section>
+      </section> */}
     </div>
   );
 }
