32ff7d0ee64cf6479d4e5ac674396fcb3a6a4b13
ouamarawl <ouamara.wail8@gmail.com>
2025-11-03 09:30:06 +0100

    footer

--- frontend/src/Components/Footer/Footer.js
+++ frontend/src/Components/Footer/Footer.js
@@ -4,56 +4,63 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <footer className="footer">
+      {/* --- ABOUT SECTION --- */}
       <div className="footer-section">
-        <h3>À PROPOS</h3>
+        <h3 data-i18n="footer_about_title">ABOUT</h3>
         <ul>
           <li>
-            <a href="#">Alain Passard</a>
+            <a href="#" data-i18n="footer_about_chef">Chef Athmane</a>
           </li>
           <li>
-            <a href="#">L'Arpège</a>
+            <a href="#" data-i18n="footer_about_restaurant">The Restaurant</a>
           </li>
           <li>
-            <a href="#">Nos Menus</a>
+            <a href="#" data-i18n="footer_about_menus">Our Menus</a>
           </li>
           <li>
-            <a href="#">Les Potagers</a>
+            <a href="#" data-i18n="footer_about_garden">The Gardens</a>
           </li>
           <li>
-            <a href="#">L'Arrière Cuisine</a>
+            <a href="#" data-i18n="footer_about_kitchen">The Back Kitchen</a>
           </li>
         </ul>
       </div>
+
+      {/* --- CONTACT SECTION --- */}
       <div className="footer-section">
-        <h3>CONTACT</h3>
+        <h3 data-i18n="footer_contact_title">CONTACT</h3>
         <ul>
           <li>
-            <a href="#">L'Arpège</a>
+            <a href="#" data-i18n="footer_contact_restaurant">The Restaurant</a>
           </li>
           <li>
-            <a href="#">L'Arrière Cuisine</a>
+            <a href="#" data-i18n="footer_contact_kitchen">The Back Kitchen</a>
           </li>
           <li>
-            <a href="#">Communication & Événements</a>
+            <a href="#" data-i18n="footer_contact_communication">Communication & Events</a>
           </li>
           <li>
-            <a href="#">Carrière</a>
+            <a href="#" data-i18n="footer_contact_career">Career</a>
           </li>
         </ul>
       </div>
+
+      {/* --- LEGAL SECTION --- */}
       <div className="footer-section">
-        <h3>INFORMATIONS LÉGALES</h3>
+        <h3 data-i18n="footer_legal_title">LEGAL INFORMATION</h3>
         <ul>
           <li>
-            <a href="#">CGU & CGV</a>
+            <a href="#" data-i18n="footer_legal_terms">Terms & Conditions</a>
           </li>
           <li>
-            <a href="#">Crédits photos</a>
+            <a href="#" data-i18n="footer_legal_photos">Photo Credits</a>
           </li>
         </ul>
       </div>
+
+      {/* --- DEVELOPERS SECTION --- */}
       <div className="footer-section">
-        <h3>développer et créer, par</h3>
+        <h3 data-i18n="footer_dev_title">Developed and Created by</h3>
         <ul>
           <li>
             <a href="#">ouamara.wail.m@gmail.com</a>
